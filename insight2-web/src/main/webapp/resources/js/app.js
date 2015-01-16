2a477e40a577a8ccd371b3bda3776a1c08c22c68
dancebutter <ken.zhangtq@gmail.com>
2015-01-15 22:37:01 -0800

    Add open source librarys. Add angular ui router. Add bootstraps styles.

---
+++ insight2-web/src/main/webapp/resources/js/app.js
@@ -0,0 +1,16 @@
+angular.module('routerApp', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            // Home state
+            .state('home', {
+                url: '/home',
+//              templateUrl: 'views/home.html'
+                templateUrl: '/index'
+            })
+            .state('stock', {
+                url: '/stock',
+                templateUrl: 'views/stock.html'
+            });
+    });
