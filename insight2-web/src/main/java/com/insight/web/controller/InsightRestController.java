643022e7a1998f351583fdf134958160ba407cd2
tli <hephltsofs@hotmail.com>
2015-01-23 08:30:25 -0500

    rest done

--- insight2-web/src/main/java/com/insight/web/controller/InsightRestController.java
+++ insight2-web/src/main/java/com/insight/web/controller/InsightRestController.java
@@ -3,10 +3,7 @@ package com.insight.web.controller;
 import generator.dao.RawPatternDao;
 import generator.model.RawPatternModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,8 +16,8 @@ public class InsightRestController {
     @Autowired
     RawPatternDao rawPatternDao;
 
-    @RequestMapping(value="sample",method = RequestMethod.GET, headers = "Accept=application/json; charset=UTF-8")
-    public @ResponseBody RawPatternModel getSampleRpm(){
+    @RequestMapping(value="sample")
+    public RawPatternModel getSampleRpm(){
         RawPatternModel rpm = (RawPatternModel) rawPatternDao.loadNext(Integer.parseInt(String.valueOf(0)));
         return rpm;
     }
