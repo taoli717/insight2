fc8697e3528111a114336e9b715f8f71c60b2c61
dancebutter <ken.zhangtq@gmail.com>
2015-01-24 19:03:38 -0800

    Use our data on high chart

--- insight2-web/src/main/webapp/resources/js/app.js
+++ insight2-web/src/main/webapp/resources/js/app.js
@@ -9,6 +9,9 @@ angular.module('routerApp', ['ui.router'])
                 templateUrl: '/resources/views/home.html',
                 controller: function($scope, $http) {
                     $scope.tableFlag = false;
+                    $scope.hideTable = function() {
+                        $scope.tableFlag = false;
+                    };
                     $scope.getSampleData = function() {
                         $http.get('api/sample')
                             .then(function(res) {
@@ -28,13 +31,92 @@ angular.module('routerApp', ['ui.router'])
                 url: '/stock',
                 templateUrl: '/resources/views/stock.html',
                 controller: function($scope, $http) {
+                    $scope.chartData = [];
+                    $scope.tableFlag = false;
+                    $scope.hideTable = function() {
+                        $scope.tableFlag = false;
+                    };
                     $scope.getSampleData = function() {
                         $http
                         .get('api/sample')
                         .then(function (res) {
                             $scope.tempSample = res.data;
-                            console.log($scope.tempSample);
+                            $scope.tableFlag = true;
+                            console.log($scope.tempSample.dailyStocks);
                             // TODO add highcharts here
+                            angular.forEach($scope.tempSample.dailyStocks, function(item) {
+                                $scope.temp = [];
+                                $scope.temp.push(item.date);
+                                $scope.temp.push(parseFloat(item.open));
+                                $scope.temp.push(parseFloat(item.high));
+                                $scope.temp.push(parseFloat(item.low));
+                                $scope.temp.push(parseFloat(item.close));
+                                //console.log($scope.temp);
+                                $scope.chartData.push($scope.temp);
+                            });
+                            console.log($scope.chartData);
+
+                            // Draw High charts
+                            $('#chartContainer').highcharts('StockChart', {
+                                chart : {
+                                    type: 'candlestick',
+                                    zoomType: 'x'
+                                },
+                                navigator : {
+                                    adaptToUpdatedData: false,
+                                    series : {
+                                        data : $scope.chartData
+                                    }
+                                },
+                                scrollbar: {
+                                    liveRedraw: false
+                                },
+                                title: {
+                                    text: 'AAPL history by the minute from 1998 to 2011'
+                                },
+                                subtitle: {
+                                    text: 'Displaying 1.7 million data points in Highcharts Stock by async server loading'
+                                },
+                                rangeSelector : {
+                                    buttons: [{
+                                        type: 'hour',
+                                        count: 1,
+                                        text: '1h'
+                                    }, {
+                                        type: 'day',
+                                        count: 1,
+                                        text: '1d'
+                                    }, {
+                                        type: 'month',
+                                        count: 1,
+                                        text: '1m'
+                                    }, {
+                                        type: 'year',
+                                        count: 1,
+                                        text: '1y'
+                                    }, {
+                                        type: 'all',
+                                        text: 'All'
+                                    }],
+                                    inputEnabled: false, // it supports only days
+                                    selected : 4 // all
+                                },
+                                xAxis : {
+/*                                    events : {
+                                        afterSetExtremes : afterSetExtremes
+                                    },
+                                    minRange: 3600 * 1000 // one hour*/
+                                },
+                                yAxis: {
+                                    floor: 0
+                                },
+                                series : [{
+                                    data : $scope.chartData,
+                                    dataGrouping: {
+                                        enabled: false
+                                    }
+                                }]
+                            });
                         });
                     };
                 }
